9ca7c2129b6dec58f0697f79372f7a81fb6c94f3
Dmytro Dovbii <dmytro.d@qualisystems.com>
2021-03-02 19:19:31 +0200

    Update entrypoint.sh

--- .github/actions/validate-bps/entrypoint.sh
+++ .github/actions/validate-bps/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+SPACE=$1
 BRANCH=${GITHUB_REF##*/}
 echo "working in branch ${BRANCH}"
 
@@ -7,7 +8,8 @@ cd blueprints || exit 1;
 
 for f in *.yaml; do
   echo "Validating ${f}..."
+  colony --token $COLONY_TOKEN --space $SPACE bp validate ${f%.yaml} -b $BRANCH  
 #  printf '%s\n' "${f%.yaml}"
 done
 
-colony --help
+# colony --help
