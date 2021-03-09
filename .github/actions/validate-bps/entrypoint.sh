f2a3540183b9470e4cac335be006e8f7e6624a1d
Dmytro Dovbii <dmytro.d@qualisystems.com>
2021-03-09 17:15:09 +0200

    test

--- .github/actions/validate-bps/entrypoint.sh
+++ .github/actions/validate-bps/entrypoint.sh
@@ -10,16 +10,26 @@ cd blueprints || exit 1;
 echo ${GITHUB_EVENT_NAME}
 echo "List of changed files:"
 #echo "CHANGED_FILES=$(git diff --name-only ${GITHUB_EVENT_BEFORE}..${GITHUB_EVENT_AFTER})"
-env
 
 err=0
-for f in *.yaml; do
-  BP=${f%.yaml}
-  echo "Validating ${BP}..."
-  colony --token $COLONY_TOKEN --space $SPACE bp validate "${BP}" --branch $BRANCH || ((err++))
-  echo "**********************************************************************"
-#  printf '%s\n' "${f%.yaml}"
-done
+
+for f in $FILES; do
+  if [[ $f == blueprints/* ]] ;
+  then
+    bpname=`echo $f | sed 's,blueprints/,,' | sed 's/.yaml//'`
+    echo "Validating ${bpname}..."
+    colony --token $COLONY_TOKEN --space $SPACE bp validate "${BP}" --branch $BRANCH || ((err++))
+  else
+    echo "Skipping file ${f}"
+  fi
+  
+# for f in *.yaml; do
+#   BP=${f%.yaml}
+#   echo "Validating ${BP}..."
+#   colony --token $COLONY_TOKEN --space $SPACE bp validate "${BP}" --branch $BRANCH || ((err++))
+#   echo "**********************************************************************"
+# #  printf '%s\n' "${f%.yaml}"
+# done
 
 echo "Number of failed blueptints: ${err}"
 
