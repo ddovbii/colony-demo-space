95eeaa9a1d06836cb0c6f1fecf2f5e5e4b028614
Dmytro Dovbii <dmytro.d@qualisystems.com>
2021-03-26 23:25:41 +0200

    test-cli

---
+++ .github/actions/start-sandbox-cli/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BRANCH=${GITHUB_REF##*/}
+
+# Compose options or command
+COLONY_OPTS="--token ${INPUT_COLONY_TOKEN} --space ${INPUT_SPACE}"
+
+if [ ${INPUT_DEBUG^^} = 'TRUE']; then
+	COLONY_OPTS+=" --debug"
+fi
+
+COLONY_START_OPTS="${INPUT_BLUEPRINT_NAME} --branch ${INPUT_BRANCH} --duration ${DURATION}"
+
+[ -n "${INPUT_SANDBOX_NAME}" ] && COLONY_START_OPTS+=" --name ${SANDBOX_NAME}"
+[ -n "${INPUT_ARTIFACTS}" ] && COLONY_START_OPTS+=" --artifacts ${INPUT_ARTIFACTS}"
+[ -n "${INPUT_INPUTS}" ] && COLONY_START_OPTS+=" --inputs ${INPUT_INPUTS}"
+
+if [ ${INPUT_TIMEOUT} != '0' ]; then
+	COLONY_START_OPTS+=" --wait ${INPUT_TIMEOUT}"
+fi
+
+sb colony $COLONY_OPTS sb start $COLONY_START_OPTS | tee log.txt
+
+SANDBOX_ID=$(cat log.txt | grep "Id:" | sed 's/Id: //')
+echo "::set-output name=sandbox-id::$(echo $SANDBOX_ID)"
