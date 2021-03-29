cf06422cb19947123e49bb757220062957e1029c
Dmytro Dovbii <dmytro.d@qualisystems.com>
2021-03-29 11:29:32 +0300

    Stop sb

---
+++ .github/actions/stop-sandbox-cli/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Compose options or command
+COLONY_OPTS="--token ${INPUT_COLONY_TOKEN} --space ${INPUT_SPACE}"
+
+if [ ${INPUT_DEBUG^^} = 'TRUE' ]; then
+	COLONY_OPTS+=" --debug"
+fi
+
+colony $COLONY_OPTS sb end $INPUT_SANDBOX_ID || echo "Unable to end sandbox"
